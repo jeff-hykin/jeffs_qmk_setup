dca0ece602892cf5bc74d98c2476c9034ed45b2f
Jeff Hykin <jeff.hykin@gmail.com>
2024-11-26 11:55:05 -0600

    formatting

--- home/keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
+++ home/keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#    include "timer.h"
+    #include "timer.h"
 #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 enum charybdis_keymap_layers {
@@ -31,15 +31,15 @@ enum charybdis_keymap_layers {
 #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
+    static uint16_t auto_pointer_layer_timer = 0;
 
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
+    #ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
+        #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
+    #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
 
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
+    #ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
+        #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
+    #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #endif     // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #define LOWER MO(LAYER_LOWER)
@@ -48,11 +48,20 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
 
 #ifndef POINTING_DEVICE_ENABLE
-#    define DRGSCRL KC_NO
-#    define DPI_MOD KC_NO
-#    define S_D_MOD KC_NO
-#    define SNIPING KC_NO
+    #define DRGSCRL KC_NO
+    #define DPI_MOD KC_NO
+    #define S_D_MOD KC_NO
+    #define SNIPING KC_NO
 #endif // !POINTING_DEVICE_ENABLE
+
+
+const uint16_t PROGMEM test_combo1[] = {KC_J, KC_U, COMBO_END};
+// const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(test_combo1, KC_D),
+    // COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+};
+
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -119,54 +128,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif // RGB_MATRIX_ENABLE
+    #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+        report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+            if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+                if (auto_pointer_layer_timer == 0) {
+                    layer_on(LAYER_POINTER);
+                    #ifdef RGB_MATRIX_ENABLE
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+                        rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+                    #endif // RGB_MATRIX_ENABLE
+                }
+                auto_pointer_layer_timer = timer_read();
+            }
+            return mouse_report;
         }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
 
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
-#        endif // RGB_MATRIX_ENABLE
-    }
-}
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+        void matrix_scan_user(void) {
+            if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+                auto_pointer_layer_timer = 0;
+                layer_off(LAYER_POINTER);
+                #ifdef RGB_MATRIX_ENABLE
+                    rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+                #endif // RGB_MATRIX_ENABLE
+            }
+        }
+    #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
-    return state;
-}
-#    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
+    #ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
+        layer_state_t layer_state_set_user(layer_state_t state) {
+            charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
+            return state;
+        }
+    #endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
 #endif     // POINTING_DEVICE_ENABLE
 
 #ifdef RGB_MATRIX_ENABLE
-// Forward-declare this helper function since it is defined in rgb_matrix.c.
-void rgb_matrix_update_pwm_buffers(void);
+    // Forward-declare this helper function since it is defined in rgb_matrix.c.
+    void rgb_matrix_update_pwm_buffers(void);
 #endif
 
 bool shutdown_user(bool) {
-#ifdef RGBLIGHT_ENABLE
-    rgblight_enable_noeeprom();
-    rgblight_mode_noeeprom(1);
-    rgblight_setrgb(RGB_RED);
-#endif // RGBLIGHT_ENABLE
-#ifdef RGB_MATRIX_ENABLE
-    rgb_matrix_set_color_all(RGB_RED);
-    rgb_matrix_update_pwm_buffers();
-#endif // RGB_MATRIX_ENABLE
+    #ifdef RGBLIGHT_ENABLE
+        rgblight_enable_noeeprom();
+        rgblight_mode_noeeprom(1);
+        rgblight_setrgb(RGB_RED);
+    #endif // RGBLIGHT_ENABLE
+    #ifdef RGB_MATRIX_ENABLE
+        rgb_matrix_set_color_all(RGB_RED);
+        rgb_matrix_update_pwm_buffers();
+    #endif // RGB_MATRIX_ENABLE
     return false;
 }
