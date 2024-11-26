9af93612bdc821f06331d7d783e770a892a0d7b0
Jeff Hykin <jeff.hykin@gmail.com>
2024-11-26 11:08:46 -0600

    -

--- bastardkb/charybdis/4x6/keymaps/mrkskk/features/os_keys.h
+++
@@ -1,9 +0,0 @@
-
-// clang-format on
-
-#pragma once
-#include QMK_KEYBOARD_H
-
-bool is_windows(void);
-bool is_mac(void);
-// static void toggle_unicode_mode(void);
--- bastardkb/charybdis/4x6/keymaps/mrkskk/readme.md
+++
@@ -1,8 +0,0 @@
-## Keymap
-- Callum style one shot mods
-- the Cmd and Ctrl keys swap when Toggle OS is pressed. 
-
-![keymap](charybdisnano-keymap2.svg)
-
-
-
